67efb7641ddcda7c460cc020cdc8d623fc689757
Daniel Shin <danielshin234@gmail.com>
2018-09-24 15:07:15 -0700

    added all other classes & layouts

--- app/src/main/java/ca/bcit/tipcalculator/MainActivity.java
+++ app/src/main/java/ca/bcit/tipcalculator/MainActivity.java
@@ -4,17 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
     }
 
-    protected void onConstraintClick(View v) {
-        Intent i = new Intent(this, ConstraintActivity.class);
-        startActivity(i);
+    protected void onConstraintClick( View v ) {
+        Intent i = new Intent( this, ConstraintActivity.class );
+        startActivity( i );
     }
+
+    protected void onLinearClick( View v ) {
+        Intent i = new Intent( this, LinearActivity.class );
+        startActivity( i );
+    }
+
+    protected void onRelativeClick( View v ) {
+        Intent i = new Intent( this, RelativeActivity.class );
+        startActivity( i );
+    }
+
+    protected void onGridClick( View v ) {
+        Intent i = new Intent( this, GridActivity.class );
+        startActivity( i );
+    }
+
+    protected void onTableClick( View v ) {
+        Intent i = new Intent( this, TableActivity.class );
+        startActivity( i );
+    }
+
 }
